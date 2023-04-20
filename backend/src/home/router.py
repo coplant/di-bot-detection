4acf668f1c7c9f7cf5119d8bd94a26706adedb4e
Ivan <omg.dayz@gmail.com>
2023-04-20 13:35:15 +0700

    add: base template

--- backend/src/home/router.py
+++ backend/src/home/router.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+from config import BASE_DIR
 
 router = APIRouter()
+templates = Jinja2Templates(directory=BASE_DIR / "src" / "templates")
 
 
-@router.get("/")
-async def root():
-    return {"message": "Hello World"}
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
 
-
-@router.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+# @router.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
 
+from config import BASE_DIR
 from src.home.router import router as home_router
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=BASE_DIR / "src" / "static"), name="static")
+
 app.include_router(home_router)
