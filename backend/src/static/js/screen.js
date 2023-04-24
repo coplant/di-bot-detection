c971c3f4a71c9bef4e50a467fac6da3503f18856
Ivan <omg.dayz@gmail.com>
2023-04-24 15:02:06 +0700

    add: window size

---
+++ backend/src/static/js/screen.js
@@ -0,0 +1,23 @@
+export function getScreenHeight() {
+    return screen.availHeight;
+}
+
+export function getScreenWidth() {
+    return screen.availWidth;
+}
+
+export function getWindowInnerHeight() {
+    return window.innerHeight;
+}
+
+export function getWindowInnerWidth() {
+    return window.innerWidth;
+}
+
+export function getWindowOuterHeight() {
+    return window.outerHeight;
+}
+
+export function getWindowOuterWidth() {
+    return window.outerWidth;
+}
