bffeb4167406d25eecdbfbe15343d56cd93cf644
Ivan <omg.dayz@gmail.com>
2023-04-29 21:10:05 +0700

    add: async client

--- proxy/src/service/router.py
+++ proxy/src/service/router.py
@@ -1,4 +1,4 @@
-import requests
+import httpx as httpx
 from fastapi import APIRouter
 from starlette.requests import Request
 from starlette.responses import Response
@@ -6,25 +6,28 @@ from starlette.responses import Response
 router = APIRouter()
 
 API_HOST = "localhost"
-PORT = "8000"
+PORT = 8000
 
 
 @router.api_route("/", methods=["GET", "POST"])
-@router.api_route("/{path}", methods=["GET", "POST"])
+@router.api_route("/{route_path:path}", methods=["GET", "POST"])
 async def proxy_route(request: Request):
-    data = await request.body()
+    body = await request.body()
     payload = dict(await request.form())
-    res = requests.request(
-        method=request.method,
-        url=str(request.url.replace(hostname=API_HOST, port=PORT)),
-        headers={k.decode(): v.decode() for k, v in request.headers.raw if
-                 k.decode().lower() in ('host', 'content-type')},
-        data=data,
-        json=payload,
-        cookies=request.cookies,
-        allow_redirects=False,
-    )
+    async with httpx.AsyncClient() as client:
+        headers = {k.decode(): v.decode() for k, v in request.headers.raw if
+                   k.decode().lower() in ('content-type', 'host')}
+        # headers["host"] = API_HOST
+        proxy_request = client.build_request(
+            method=request.method,
+            url=str(request.url.replace(hostname=API_HOST, port=PORT)),
+            headers=headers,
+            content=body,
+            data=payload,
+            cookies=request.cookies
+        )
+        response = await client.send(proxy_request, follow_redirects=True)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-    headers = {k: v for k, v in res.raw.headers.items() if k.lower() not in excluded_headers}
-    response = Response(content=res.content, status_code=res.status_code, headers=headers)
+    headers = {k.decode(): v.decode() for k, v in response.headers.raw if k.decode().lower() not in excluded_headers}
+    response = Response(content=response.content, status_code=response.status_code, headers=headers)
     return response
