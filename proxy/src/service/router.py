311ec0a046d5fb84ce8377c96b8395b95309b5e2
Ivan <omg.dayz@gmail.com>
2023-04-30 14:09:12 +0700

    add: JS response

--- proxy/src/service/router.py
+++ proxy/src/service/router.py
@@ -1,16 +1,37 @@
 import httpx as httpx
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from starlette.requests import Request
-from starlette.responses import Response
-
+from starlette.responses import Response, HTMLResponse
 from proxy.src.config import API_HOST, API_PORT
 
 router = APIRouter()
+STATIC_PATH = "/static/js/core.js"
+
+
+@router.post("/api/json")
+async def get_fingerprint():
+    # todo: receive fingerprint
+    return NotImplemented
 
 
 @router.api_route("/", methods=["GET", "POST"])
 @router.api_route("/{route_path:path}", methods=["GET", "POST"])
 async def proxy_route(request: Request):
+    cookie = request.cookies.get("sessionIdentifier")
+    if not cookie:
+        html_content = f"""<script type="module" src="{STATIC_PATH}"></script>"""
+        return HTMLResponse(content=html_content, media_type="text/html")
+
+    # todo: сделать разветвление
+    #   1: если кук нет - рендерить HTML + JS,
+    #       JS отправляет данные на роут,
+    #       роут устанавливает куки
+    #       куки сообщают о том, что пользователь отправил свои отпечатки => в JS делать редирект или обновление
+    #   2: если есть куки - делать проксирование
+    #       куки должны быстро истекать? живут до закрытия браузера
+    #       в первом роуте делать запрос в DB по IP
+    #       если кука не соответствует пользователю - запрос на получение отпечатков
+
     body = await request.body()
     payload = dict(await request.form())
     async with httpx.AsyncClient() as client:
@@ -25,7 +46,7 @@ async def proxy_route(request: Request):
             data=payload,
             cookies=request.cookies
         )
-        response = await client.send(proxy_request, follow_redirects=True)
+        response = await client.send(proxy_request, follow_redirects=False)
     excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
     headers = {k.decode(): v.decode() for k, v in response.headers.raw if k.decode().lower() not in excluded_headers}
     response = Response(content=response.content, status_code=response.status_code, headers=headers)
