8ffdb6a621419aac89fb2948df763cb6dd22748d
Ivan <omg.dayz@gmail.com>
2023-05-01 16:46:05 +0700

    add: utils

--- proxy/src/service/utils.py
+++ proxy/src/service/utils.py
@@ -1,10 +1,40 @@
-from bs4 import BeautifulSoup as Soup
+from datetime import datetime, timedelta
+from enum import Enum
+from uuid import UUID
 
-from service import router
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.requests import Request
+
+from service import models
+from src.config import EXPIRATION_TIME
 
 
-def prepare_html(content):
-    html = Soup(content, "lxml")
-    for a in html.find_all("a", href=True):
-        a['href'] = a['href'].replace(router.API_HOST.lower(), "")
-    return html
+class Actions(Enum):
+    FP_received = 0
+    FP_analyzed = 1
+    Cookie_sent = 2
+    Cookie_received = 3
+    Cookie_invalid = 4
+    Bod_detected = 5
+
+
+def generate_valid_until():
+    valid_until = datetime.utcnow() + timedelta(seconds=EXPIRATION_TIME)
+    return valid_until
+
+
+def is_valid_uuid(client_uuid, version=4):
+    try:
+        uuid_obj = UUID(client_uuid, version=version)
+    except ValueError:
+        return False
+    return str(uuid_obj) == client_uuid
+
+
+async def as_bot(request: Request, session: AsyncSession):
+    user = models.User(ip=request.client.host, is_bot=True)
+    session.add(user)
+    await session.flush()
+    log = models.Logging(user_id=user.id, action_id=Actions.Bod_detected.value)
+    session.add(log)
+    await session.commit()
