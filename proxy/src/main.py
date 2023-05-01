e0b91ed9999b59c8b967848d083d0a5332e2009e
Ivan <omg.dayz@gmail.com>
2023-05-01 16:45:37 +0700

    add: request check ip middleware

--- proxy/src/main.py
+++ proxy/src/main.py
@@ -4,6 +4,8 @@ import uvicorn
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 
+from service.middlewares import validate_ip
+
 sys.path.append("..")
 
 from config import BASE_DIR
@@ -12,6 +14,7 @@ from service.router import router as service_router
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=BASE_DIR / "src" / "static"), name="static")
 app.include_router(service_router)
+app.middleware("http")(validate_ip)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=7000)
---
+++ proxy/src/service/middlewares.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request, status, Depends
+from fastapi.responses import Response
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import async_session_maker
+from service.models import User
+
+
+async def validate_ip(request: Request, call_next):
+    client_ip = str(request.client.host)
+    async with async_session_maker() as session:
+        query = select(User).filter_by(ip=client_ip).filter_by(is_bot=True)
+        result = await session.execute(query)
+        result = result.scalars().all()
+    if client_ip in [user.ip for user in result]:
+        return Response(status_code=status.HTTP_403_FORBIDDEN)
+    return await call_next(request)
