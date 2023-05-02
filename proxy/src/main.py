d8798487e4f55771f3af616dd3e47c0e3f32db08
Ivan <omg.dayz@gmail.com>
2023-05-02 23:37:17 +0700

    fix: clean up main

--- proxy/src/main.py
+++ proxy/src/main.py
@@ -1,17 +1,14 @@
-import sys
 import uvicorn
 from fastapi import FastAPI
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 from starlette.staticfiles import StaticFiles
-from exceptions import rate_limit_exceeded_handler
-from ratelimiter import limiter
 
-from proxy.src.service.middlewares import validate_ip, validate_fingerprint
+from proxy.src.exceptions import rate_limit_exceeded_handler
+from proxy.src.ratelimiter import limiter
+from proxy.src.config import BASE_DIR
 from proxy.src.service.router import router as service_router
-
-
-from config import BASE_DIR
+from proxy.src.service.middlewares import validate_ip, validate_fingerprint
 
 app = FastAPI()
 
